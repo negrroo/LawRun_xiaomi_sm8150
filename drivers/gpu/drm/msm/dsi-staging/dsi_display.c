bca8bb6cc630b9b2fb0621a63e44a9a751717c63
Midas Chien <midaschieh@google.com>
2018-10-26 16:39:49 +0800

    drm/msm/dsi-staging: return suitable value in debugfs write function
    Return value of write function is the number of bytes written. Zero
    indicates nothing was written. In this case write function may be called again
    then enter infinite lopp. Return suitable errno when operation is not
    success.
    
    Bug: 118473677
    Change-Id: Icc4f228ab01e20a30d558dc25d6d177e5ee54cbb
    Signed-off-by: Midas Chien <midaschieh@google.com>
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -1356,7 +1356,7 @@ static ssize_t debugfs_misr_setup(struct file *file,
 		return -ENODEV;
 
 	if (*ppos)
-		return 0;
+		return -EINVAL;
 
 	buf = kzalloc(MISR_BUFF_SIZE, GFP_KERNEL);
 	if (!buf)
@@ -1485,7 +1485,7 @@ static ssize_t debugfs_esd_trigger_check(struct file *file,
 		return -ENODEV;
 
 	if (*ppos)
-		return 0;
+		return -EINVAL;
 
 	if (user_len > sizeof(u32))
 		return -EINVAL;
@@ -1624,7 +1624,7 @@ static ssize_t debugfs_read_esd_check_mode(struct file *file,
 		return -ENODEV;
 
 	if (*ppos)
-		return 0;
+		return -EINVAL;
 
 	if (!display->panel) {
 		pr_err("invalid panel data\n");
@@ -1643,7 +1643,8 @@ static ssize_t debugfs_read_esd_check_mode(struct file *file,
 	}
 
 	if (!esd_config->esd_enabled) {
-		rc = snprintf(buf, len, "ESD feature not enabled");
+		pr_warn("esd check didn't enable\n");
+		rc = -EINVAL;
 		goto output_mode;
 	}
 
